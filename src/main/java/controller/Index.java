834f60107542c9634d18fdc79297449d4768854b
tianya20180 <1320249650@qq.com>
2021-01-20 20:03:35 +0800

    初始化spring-boot

---
+++ src/main/java/controller/Index.java
@@ -0,0 +1,17 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Index {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "helloword!";
+    }
+}
